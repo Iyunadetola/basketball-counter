dc6e08666323447d88eb8f22c6a523c557b63de6
iyunadetola <aderetioluwatoyin@gmail.com>
2022-09-23 01:29:38 +0100

    A basketball score counter

---
+++ basketball.js
@@ -0,0 +1,34 @@
+let homeIncr = document.getElementById('home-incr')
+let guestIncr = document.getElementById('guest-incr')
+count = 0
+function increment1 (){
+    count += 1
+    homeIncr.textContent= count
+    
+}
+
+function increment2 (){
+    count += 2
+    homeIncr.textContent= count
+    
+}
+function increment3 (){
+    count += 3
+    homeIncr.textContent= count
+    
+}
+function incrementt1 (){
+    count += 1
+    guestIncr.textContent= count
+    
+}
+function incrementt2 (){
+    count += 2
+    guestIncr.textContent= count
+    
+}
+function incrementt3 (){
+    count += 3
+    guestIncr.textContent= count
+    
+}
